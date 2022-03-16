8c676cabb771a6e834fb4e44aa515c1b2734d327
CMM <chrystellemm@gmail.com>
2022-03-16 16:18:30 +0100

    fonction CRUD

--- app/models/voiture.js
+++ app/models/voiture.js
@@ -1,43 +1,27 @@
-const mongoose = require('mongoose');
-const { DATETIME } = require('mysql/lib/protocol/constants/types');
-const Schema = mongoose.Schema;
+const sql = require("./db.js");
+const Voiture = function(Voiture){
 
-const voiture = new Schema(
-    {
-        immatriculation : {
-            type: String
-        },
-        nombrePlace : {
-            type: Number
-        },
-        boite : {
-            type: String
-        },
-         annee :{
-            type: DATETIME
-        },
-        carburant :{
-            type: String
-        },
-        marque_id :{
-            type: Number
-        },
-        modele_id :{
-            type: Number
-        },
-        statut_id :{
-            type: Number
-        },
-        categorie_id :{
-            type: Number
-        }
+    this.immatriculation = this.immatriculation.Voiture;
+    this.nbrePlace = this.nbrePlace.Voiture;
+    this.boite = this.boite.Voiture;
+    this.annee = this.annee.Voiture;
+    this.carbutant = this.carbutant.Voiture;
+    this.marque_id = this.marque_id.Voiture;
+    this.modele_id = this.modele_id.Voiture;
+    this.statut_id  = this.statut_id.Voiture;
+    this.categorie_id = this.categorie_id.Voiture
+}
 
+Voiture.create = (newVoiture, result) => {
+    sql.query("INSERT INTO voiture SET ?", newVoiture, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+      console.log("created voiture: ", { id: res.insertId, ...newTutorial });
+      result(null, { id: res.insertId, ...newTutorial });
+    });
+  };
 
-    },
-    {
-        collection: 'voiture'
-    }
-
-
-);
-module.exports = mongoose.model('voiture', voiture);
+  
