56ec35b17683f6aff9743a540db4c8924ca96e3d
CMM <chrystellemm@gmail.com>
2022-04-12 15:30:56 +0200

    authentification 2

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,5 +2,18 @@ module.exports = {
     HOST: "localhost",
     USER: "root",
     PASSWORD: "",
-    DB: "mycar"
+    DB: "mycar",
+    dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+
   };
+
+//Pour Sequelize max: nombre maximum de connexions dans le pool
+//min: nombre minimum de connexion dans le pool
+//idle: durée maximale, en millisecondes, pendant laquelle une connexion peut être inactive avant d’être libérée
+//acquire: durée maximale, en millisecondes, pendant laquelle le pool tentera d’obtenir la connexion avant de lancer l’erreur
