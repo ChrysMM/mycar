20d0ecb8dfec69876d840a4341d8475c56ae7ab0
lombardo <ornellalombardo312@gmail.com>
2022-05-10 09:36:45 +0200

    comm

--- mycar_Front/App.js
+++ mycar_Front/App.js
@@ -12,6 +12,21 @@ import  Detail from './components/Detail';
 import  Historique from './components/Historique';
 import  Oublie from './components/Oublie';
 import  Valider from './components/Valider';
+
+
+const express = require("express");
+const app = express();
+  
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+  
+const PORT = process.env.PORT || 8080;
+  
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+
+
 const AppStack = createStackNavigator();
 export default function Navigator(){
  
