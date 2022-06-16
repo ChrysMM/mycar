66417526b601fa4af04a3812394c7b939801d4f1
CMM <chrystellemm@gmail.com>
2022-06-16 16:21:56 +0200

    Username connexion

--- mycar_Front/App.js
+++ mycar_Front/App.js
@@ -1,51 +1,95 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import  Profil from './components/Profil';
-import  Filtre from './components/Filtre';
-import  Search from './components/Search';
-import  Calendrier from './components/Calendrier';
-import  Confirmation from './components/Confirmation';
-import  Connexion from './components/Connexion';
-import  date from './components/date';
-import  Detail from './components/Detail';
-import  Historique from './components/Historique';
-import  Oublie from './components/Oublie';
-import  Valider from './components/Valider';
-import  Camera from './components/Camera';
-import axios from 'axios';
+import React, { Component, component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Profil from "./components/Profil";
+import Filtre from "./components/Filtre";
+import Search from "./components/Search";
+import Calendrier from "./components/Calendrier";
+import Confirmation from "./components/Confirmation";
+import Connexion from "./components/Connexion";
+import date from "./components/date";
+import Detail from "./components/Detail";
+import Historique from "./components/Historique";
+import Oublie from "./components/Oublie";
+import Valider from "./components/Valider";
+import Camera from "./components/Camera";
+import axios from "axios";
 
-const URL = "http://localhost:8080/api/auth/signin"
- 
-
-
-
+//(adresse ip ordi)  sue wifi connexion tel
+//const URL = "http://172.20.10.4:8080/api/auth/signin"
+const URL = "http://localhost:8080/api/auth/signin";
 
 const AppStack = createStackNavigator();
-export default function Navigator(){
-    axios({method:'post', url:URL, data:{username: "Legrant", password:"234mlkj"}}).then((response) => {
-        console.log(response.data);
-        
-    }).catch((err)=>{
-        console.log("error", err);
-    }); 
-    return (
-    <NavigationContainer>
-    <AppStack.Navigator screenOptions={{ headerShown: true }} >
 
-    <AppStack.Screen name="Connexion" component={Connexion} />
-    <AppStack.Screen name="Oublie" component={Oublie} />
-    <AppStack.Screen name="date" component={date} />
-    <AppStack.Screen name="Detail" component={Detail} />
-    <AppStack.Screen name="Search" component={Search} />
-    <AppStack.Screen name="Filtre" component={Filtre} />
-    <AppStack.Screen name="Valider" component={Valider} />
-    <AppStack.Screen name="Profil" component={Profil} />
-    <AppStack.Screen name="Calendrier" component={Calendrier} />
-    <AppStack.Screen name="Confirmation" component={Confirmation} />
-    <AppStack.Screen name="Historique" component={Historique} />
-    </AppStack.Navigator>
+export default function Navigator() {
+  axios({
+    method: "post",
+    url: URL,
+    data: { username: "Legrant", password: "234mlkj" },
+  })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => {
+      console.log("error", err);
+    });
+
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: true }}>
+        <AppStack.Screen name="Connexion" component={Connexion} />
+        <AppStack.Screen name="Oublie" component={Oublie} />
+        <AppStack.Screen name="date" component={date} />
+        <AppStack.Screen name="Detail" component={Detail} />
+        <AppStack.Screen name="Search" component={Search} />
+        <AppStack.Screen name="Filtre" component={Filtre} />
+        <AppStack.Screen name="Valider" component={Valider} />
+        <AppStack.Screen name="Profil" component={Profil} />
+        <AppStack.Screen name="Calendrier" component={Calendrier} />
+        <AppStack.Screen name="Confirmation" component={Confirmation} />
+        <AppStack.Screen name="Historique" component={Historique} />
+      </AppStack.Navigator>
     </NavigationContainer>
-    
-    );
+  );
 }
+
+// }
+// import React from 'react';
+// import axios from 'axios';
+
+// export default class PersonList extends React.Component {
+//   state = {
+//     name: '',
+//   }
+
+//   handleChange = event => {
+//     this.setState({ name: event.target.value });
+//   }
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+
+//     const user = {
+//       name: this.state.name
+//     };
+
+//     axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
+//       .then(res => {
+//         console.log(res);
+//         console.log(res.data);
+//       })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <form onSubmit={this.handleSubmit}>
+//           <label>
+//             Person Name:
+//             <input type="text" name="name" onChange={this.handleChange} />
+//           </label>
+//           <button type="submit">Add</button>
+//         </form>
+//       </div>
+//     )
+//   }
--- mycar_Front/components/Connexion.js
+++ mycar_Front/components/Connexion.js
@@ -1,88 +1,101 @@
 import React from "react";
-import { TextInput, View, Text, StyleSheet} from 'react-native'
-import { useNavigation } from '@react-navigation/native';
-
- 
+import { TextInput, View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
  
 
-//const URL = "http://localhost:8080/api/auth/signin"
+//(adresse ip ordi)  sue wifi connexion tel
+//const URL = "http://172.20.10.4:8080/api/auth/signin"
+const URL = "http://localhost:8080/api/auth/signin";
 
 export default function Connexion() {
-    //axios({method:'get', url:URL}).then((response) => {
-    //     console.log(response.data);
-    // }).catch((err)=>{
-    //     console.log("error", err);
-    // }); 
-    const navigation = useNavigation();
-    function navigateConnexion() {
-        navigation.navigate("date");
-    }
-    function navigateOublie() {
-        navigation.navigate("Oublie");
-    }
-        return(
-<View style={style.view}>
-    <Text style={style.titleid} >Renseignez vos identifiants</Text>
-    <TextInput style={style.title} placeholder="E-mail"></TextInput>
+  const navigation = useNavigation();
 
-    <TextInput style={style.title} placeholder="Mot de passe"></TextInput>
-
-    <Text style={style.oublie} onPress={() => navigateOublie()}>Mot de passe oublié? </Text>
-
-    <View>
-    <Text style={style.connexion}  onPress= {()=>navigateConnexion()}> Connexion</Text>
-    </View>
-</View>
-            ); 
-        }
-    
-    
-    const style =StyleSheet.create({
-        view: {
-            alignItems: "center", 
-        },
-        titleid: {
-            fontSize: 22,
-            fontWeight: "bold",
-            textAlign: 'center',  
-            marginTop: 70, 
-            marginBottom: 70, 
-        }, 
-    title: {
-        fontSize: 14, 
-        marginBottom: 3, 
-        marginTop: 10, 
-        backgroundColor: '#F5F5F5', 
-        height: 55, 
-        borderRadius: 10, 
-        fontWeight: "bold", 
-        width: 350, 
-        marginLeft: 7, 
-        fontStyle: 'italic', 
-        alignItems: 'center'
    
-       
-    }, 
 
-    oublie: {
-        color: '#A2273C', 
-        fontStyle: 'italic', 
-        marginTop: 10,
-        marginLeft: 10,  
-    }, 
 
-    connexion: {
-        backgroundColor: '#A2273C', 
-        textAlign: "center", 
-        width: 120, 
-        height:  30, 
-        borderRadius: 7,
-        elevation: 3,
-        marginTop: 58, 
-        color: 'white', 
-        fontWeight: "bold",
-        paddingTop: 4, 
-        paddingRight: 3, 
-        alignItems: 'center'
-    }, 
-    })
+
+  function navigateConnexion() {
+     
+
+
+
+    navigation.navigate("date");
+  }
+
+
+  function navigateOublie() {
+    navigation.navigate("Oublie");
+  }
+
+  return (
+    <View style={style.view}>
+      <Text style={style.titleid}>Renseignez vos identifiants</Text>
+      <TextInput style={style.title} 
+                 placeholder="Username">
+
+                 </TextInput>
+
+      <TextInput style={style.title} 
+                 placeholder="Mot de passe"></TextInput>
+
+      <Text style={style.oublie} onPress={() => navigateOublie()}>
+        Mot de passe oublié?
+      </Text>
+
+      <View>
+        <Text style={style.connexion} onPress={() => navigateConnexion()}>
+          {" "}
+          Connexion
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  view: {
+    alignItems: "center",
+  },
+  titleid: {
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 70,
+    marginBottom: 70,
+  },
+  title: {
+    fontSize: 14,
+    marginBottom: 3,
+    marginTop: 10,
+    backgroundColor: "#F5F5F5",
+    height: 55,
+    borderRadius: 10,
+    fontWeight: "bold",
+    width: 350,
+    marginLeft: 7,
+    fontStyle: "italic",
+    alignItems: "center",
+  },
+
+  oublie: {
+    color: "#A2273C",
+    fontStyle: "italic",
+    marginTop: 10,
+    marginLeft: 10,
+  },
+
+  connexion: {
+    backgroundColor: "#A2273C",
+    textAlign: "center",
+    width: 120,
+    height: 30,
+    borderRadius: 7,
+    elevation: 3,
+    marginTop: 58,
+    color: "white",
+    fontWeight: "bold",
+    paddingTop: 4,
+    paddingRight: 3,
+    alignItems: "center",
+  },
+});
