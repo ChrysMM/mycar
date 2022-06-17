bd906b699561d1bb7d26c39b7da93dc34c09df9b
lombardo <ornellalombardo312@gmail.com>
2022-06-17 11:37:16 +0200

    modification_afffichage

--- mycar_Front/components/Search.js
+++ mycar_Front/components/Search.js
@@ -17,11 +17,13 @@ import axios from 'axios';
 
 // const tab = createBottomTabNavigator(); 
 const URL = "http://localhost:8080/api/voiture"; 
-
+const URLmodele= "http://localhost:8080/api/modele";
+const URLmarque= "http://localhost:8080/api/marque"
 
 export default function Search() {
     const [get, setGet] = React.useState(null);
-
+    const [get2, setGet2] = React.useState(null);
+    const [get4, setGet4] = React.useState(null);
     React.useEffect(() => {
     axios({method:'get', url:URL}).then( (response) => {
         setGet(response.data);
@@ -30,9 +32,27 @@ export default function Search() {
     }).catch((err)=>{
         console.log("error", err);
     });
+
+    axios({method:'get', url:URLmodele}).then( (response) => {
+        setGet2(response.data);
+        
+        console.log("connexion test");
+    }).catch((err)=>{
+        console.log("error", err);
+    });
+
+    axios({method:'get', url:URLmarque}).then( (response) => {
+        setGet4(response.data);
+        
+        console.log("connexion test");
+    }).catch((err)=>{
+        console.log("error", err);
+    });
+
   }, []);
-  
   if (!get) return null;
+  if (!get2) return null;
+  if (!get4) return null;
 
     const navigation = useNavigation();
 
@@ -91,7 +111,7 @@ export default function Search() {
 
         <View style={style.bloc}>
 
-        <Text style={style.title} onPress= {()=>navigateSearch()} >{get[i].modele_id}</Text>
+        <Text style={style.title} onPress= {()=>navigateSearch()} > {get4[i].nomMarque} {get2[i].nomModele} <br /></Text>
           <Text style={style.text}><Image source={ personne }   style={style.logobande} ></Image>{get[i].nbrePlace}  </Text>
         <Text style={style.text}><Image source={ boiteA }   style={style.logobande} ></Image> {get[i].boite}    </Text>
         
