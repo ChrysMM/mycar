d5564cc6182222992e948c3c5e6dfb79db03eafd
CMM <chrystellemm@gmail.com>
2022-08-01 09:10:28 +0200

    Login

--- mycar_Front/App.js
+++ mycar_Front/App.js
@@ -1,5 +1,4 @@
- 
-// import React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import  Profil from './components/Profil';
@@ -16,20 +15,19 @@ import  Valider from './components/Valider';
 // import Listing from './Components/Listing'; 
 
 
-// const AppStack = createStackNavigator();
- 
+const AppStack = createStackNavigator();
  
 export default function Navigator(){
-    
+
     return (
     <NavigationContainer>
     <AppStack.Navigator screenOptions={{ headerShown: true }} >
-    <AppStack.Screen options={{headerShown: false}} name="LoginView" component={LoginView} options={{ tabBarBadge: 3 }}/>
-    {/* <AppStack.Screen options={{headerShown: false}} name="Listing" component={Listing} /> */}
+     <AppStack.Screen options={{headerShown: false}} name="LoginView" component={LoginView} options={{ tabBarBadge: 3 }}/>
+
     <AppStack.Screen options={{headerShown: false}} name="Oublie" component={Oublie} />
     <AppStack.Screen options={{headerShown: false}} name="date" component={date} />
     <AppStack.Screen options={{headerShown: false}} name="Detail" component={Detail} />
-    <AppStack.Screen options={{headerShown: false}} name="Calendrier" component={Calendrier} />
+    <AppStack.Screen options={{headerShown: false}} name="Calendrier" component={Calendrier} /> 
     <AppStack.Screen options={{headerShown: false}} name="Search" component={Search} />
     <AppStack.Screen options={{headerShown: false}} name="Filtre" component={Filtre} />
     <AppStack.Screen options={{headerShown: false}} name="Valider" component={Valider} />
@@ -39,40 +37,5 @@ export default function Navigator(){
     </AppStack.Navigator>
     </NavigationContainer>
   );
-} 
+}
  
-
-import React from 'react';
-
-import {
-  SafeAreaView,
-  StyleSheet,
-  KeyboardAvoidingView,
-  StatusBar,
-} from 'react-native';
-
-import LoginView from './components/LoginView';
-
-const App = () => {
-  return (
-    <KeyboardAvoidingView
-     // behavior={Platform.OS === 'ios' ? 'padding' : 'position'}
-      style={{flex: 1}}
-      enabled>
-      <StatusBar barStyle="dark-content" />
-
-      <SafeAreaView style={styles.container}>
-        <LoginView />
-      </SafeAreaView>
-    </KeyboardAvoidingView>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f6f6f6"
-  },
-});
-
-export default App;
