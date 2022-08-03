bfd9753960fe434b5f055d31f4b0dc7b3c633fd3
lombardo <ornellalombardo312@gmail.com>
2022-08-03 10:46:42 +0200

    axios

--- logiciel/main.js
+++ logiciel/main.js
@@ -5,9 +5,21 @@ const path = require('path')
 
 
 const server = require("../app");
+const axios= require("axios");
 
 
+//Axios qui permet l'acces a la base de données
 
+const AxiosVoiture = () => {
+    axios.get('http://localhost:8080/api/voiture')
+        .then(response => {
+            const voiture = response.data.data;
+            console.log("connexion test", voiture);
+        })
+        .catch(error => console.error("error"));
+};  
+
+AxiosVoiture();
 
 // fonction qui va permettre de créer une fenetre
 function createWindow () {
